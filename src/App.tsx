754fe844915fbed5fb4935a3798968cf6d842cc9
Glauber Jesus <glauberjds1@hotmail.com>
2021-06-17 01:43:33 -0300

    Primeiro Commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+import { MoviesProvider} from './useContextMovies';
+
+import './styles/global.scss';
+
+import './styles/sidebar.scss';
+import './styles/content.scss';
+
+
+export function App() {
+  return (
+      <MoviesProvider>
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar/>
+      <Content/>
+    </div>
+      </MoviesProvider>
+  )
+}
---
+++ src/components/Content.tsx
@@ -0,0 +1,25 @@
+
+import { MovieCard } from './MovieCard';
+import { useMovies } from '../useContextMovies';  
+
+export function Content() {
+  const { movies, selectedGenre} = useMovies();
+ 
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
+}
+
+
